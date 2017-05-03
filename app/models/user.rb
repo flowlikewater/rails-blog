b944a47a1bafc4aa424a97a1359c18f01808da21
Kevin <cho.kevin@hotmail.com>
2017-05-03 16:41:56 +0800

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-     
+  validates :username, presence: true,
+            uniqueness: { case_sensitive: false },
+            length: {minimum: 3, maximum: 25}
+  # http://guides.rubyonrails.org/active_record_validations.html#validation-helpers
+  VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: {maximum: 105},
+            uniqueness: {case_sensitive: false},
+            format: {with: VALID_EMAIL_REGEX}
 end
